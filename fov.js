7a4972cbfd52d6133514ea0a3465f1732399ffa7
Juarez Bochi <jbochi@gmail.com>
2011-04-24 14:43:38 -0300

    Added Arc subtraction

--- fov.js
+++ fov.js
@@ -1,8 +1,51 @@
-exports.Arc = function (start, end) {
+Arc = function (start, end) {
 	this.start = start;
 	this.end = end;
 };
 
-exports.Arc.prototype.contains = function (angle) {
+Arc.prototype.contains = function (angle) {
 	return ((this.start < angle) && (angle < this.end));
 };
+
+Arc.prototype.equal = function (arc) {
+	return ((this.start === arc.start) && (this.end === arc.end));
+};
+
+Arc.prototype.sub = function (remove) {
+	if ((remove.start > this.start) && (remove.end < this.end)) {
+	    //Arc is split in two
+	    return [new exports.Arc(this.start, remove.start), 
+		        new exports.Arc(remove.end, this.end)];
+	} else if ((remove.start <= this.start) && (remove.end >= this.end)) {
+		//Nothing left
+        return;
+    } else {
+		var a;
+		if ((remove.start > this.end) || (remove.end < this.start)) {
+    		//Nothing changes
+		    a = this;
+		} else if ((remove.start > this.start) && 
+		           (remove.end >= this.end)) {
+	        // Upper part is removed
+	        a = new Arc(this.start, remove.start);
+        } else if ((remove.start <= this.start) &&
+		           (remove.end < this.end)) {
+	        //Lower part is removed
+	        a = new Arc(remove.end, this.end);
+		}
+
+	    if ((remove.start < 0) && (this.start > 0)) {
+			remove.start += 2 * Math.PI;
+			remove.end += 2 * math.PI;
+			a = a.sub(remove);
+		} else if ((remove.start > 0) && (this.start < 0)) {
+			remove.start -= 2 * Math.PI;
+			remove.end -= 2 * Math.PI;
+			a = a.sub(remove);
+		}
+
+	    return a;
+	}
+};
+
+exports.Arc = Arc;
--- test/fov-test.js
+++ test/fov-test.js
@@ -3,7 +3,7 @@ var assert = require('assert'),
     Arc = require('../fov').Arc;
 
 vows.describe('Arc').addBatch({
-	'Arc (0 - PI)': {
+	'Arc(0, PI)': {
 		topic: function () {
 			return new Arc(0, Math.PI); 
 		},
@@ -15,6 +15,16 @@ vows.describe('Arc').addBatch({
 		},
 		'should contain PI/2': function (topic) {
 			assert.isTrue(topic.contains(Math.PI/2));
+		},
+		'should not contain 1.5 PI': function (topic) {
+			assert.isFalse(topic.contains(1.5 * Math.PI));
+		},
+		'should be equal to self': function (topic) {
+			assert.isTrue(topic.equal(topic));
+		},
+		'minus (PI/2, PI) is (0, PI/2) ': function (topic) {
+			var sub = new Arc(Math.PI / 2, Math.PI);
+			assert.isTrue(topic.sub(sub).equal(new Arc(0, Math.PI/2)));
 		},
 	},
 }).export(module);
